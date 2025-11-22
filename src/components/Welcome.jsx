44dceef3d62cece6cd43b9ceecd3c11df189e1f7
= <harifin857@outlook.com>
2025-11-22 16:49:04 +0700

    implement welcome section

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,94 @@
+import React, { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const FONT_WEIGHTS = {
+  subtitle: { min: 100, max: 400, default: 100 },
+  title: { min: 400, max: 900, default: 400 },
+};
+
+const renderText = (text, className, baseWeight = 400) => {
+  return [...text].map((char, i) => (
+    <span
+      key={i}
+      className={className}
+      style={{ fontVariationSettings: `'wght' ${baseWeight}` }}
+    >
+      {char == " " ? "\u00A0" : char}
+    </span>
+  ));
+};
+
+const setupTextHover = (container, type) => {
+  if (!container) return;
+  const letters = container.querySelectorAll("span");
+  const { min, max, default: base } = FONT_WEIGHTS[type];
+
+  const animateLetter = (letter, weight, duration = 0.25) => {
+    return gsap.to(letter, {
+      duration,
+      ease: "power2.out",
+      fontVariationSettings: `'wght' ${weight}`,
+    });
+  };
+  const handleMouseMove = (e) => {
+    const { left } = container.getBoundingClientRect();
+    const mouseX = e.clientX - left;
+
+    letters.forEach((letter) => {
+      const { left: l, width: w } = letter.getBoundingClientRect();
+      const distance = Math.abs(mouseX - (l - left + w / 2));
+      const intensity = Math.exp(-(distance ** 2) / 20000);
+      animateLetter(letter, min + (max - min) * intensity);
+    });
+  };
+
+  const handleMouseLeave = () => {
+    letters.forEach((letter) => animateLetter(letter, base, 0.3));
+  };
+
+  container.addEventListener("mousemove", handleMouseMove);
+  container.addEventListener("mouseleave", handleMouseLeave);
+  return () => {
+    container.removeEventListener("mousemove", handleMouseMove);
+    container.removeEventListener("mouseleave", handleMouseLeave);
+  };
+};
+
+const Welcome = () => {
+  const titleRef = useRef(null);
+  const subtitleRef = useRef(null);
+
+  useGSAP(() => {
+    const titleCleanup = setupTextHover(titleRef.current, "title");
+    const subtitleClenup = setupTextHover(subtitleRef.current, "subtitle");
+
+    return () => {
+      titleCleanup();
+      subtitleClenup();
+    };
+  }, []);
+  return (
+    <section id="welcome">
+      <p ref={subtitleRef}>
+        {renderText(
+          "Hey I'm Yogi! Welcome to my",
+          "text-3xl font-georama",
+          100
+        )}
+      </p>
+      <h1 ref={titleRef} className="mt-7">
+        {renderText(
+          "Portfolio",
+          "text-9xl font-georama italic tracking-wider",
+          400
+        )}
+      </h1>
+      <div className="small-screen">
+        <p>This Portfolio is designed for desktop/tabled screens only.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Welcome;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import Navbar from "./Navbar";
+import Welcome from "./Welcome";
+export { Navbar, Welcome };
